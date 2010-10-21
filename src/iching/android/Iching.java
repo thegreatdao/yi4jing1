d19eb241620400494f81040190d733ad59297111
HL <anotherdayinpara@yahoo.com>
2010-10-21 12:00:10 -0400

    first added files

---
+++ src/iching/android/Iching.java
@@ -0,0 +1,14 @@
+package iching.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Iching extends Activity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
