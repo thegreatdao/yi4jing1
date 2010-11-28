d93b5e920db2af33b7718562ecd9372c0eaf2afb
hl <everbeyond@hotmail.com>
2010-11-28 13:55:28 -0500

    refactored some code

--- src/iching/android/activities/CastIChing.java
+++ src/iching/android/activities/CastIChing.java
@@ -18,7 +18,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
