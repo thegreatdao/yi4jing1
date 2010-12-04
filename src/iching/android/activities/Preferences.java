12d13b72a06e21def6e69eececb6349c87ad1479
hl <everbeyond@hotmail.com>
2010-12-04 17:39:34 -0500

    finished Preferece

--- src/iching/android/activities/Preferences.java
+++ src/iching/android/activities/Preferences.java
@@ -6,9 +6,9 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
-import android.widget.Toast;
 
 public class Preferences extends PreferenceActivity implements OnSharedPreferenceChangeListener
 {
@@ -18,15 +18,35 @@ public class Preferences extends PreferenceActivity implements OnSharedPreferenc
 	public static final String DEFAULT_VALUE_VIEW = "0";
 	public static final String KEY_DIVINATIONS = "numOfRecordsPref";
 	public static final String DEFAULT_VALUE_DIVINATIONS = "10";
+	
+	private ListPreference viewListPreference;
+	private ListPreference numOfRecordsListPreference;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.settings);
-		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+		viewListPreference = (ListPreference)getPreferenceScreen().findPreference(KEY_VIEW);
+		numOfRecordsListPreference = (ListPreference)getPreferenceScreen().findPreference(KEY_DIVINATIONS);
+		setUpViewSummary(KEY_VIEW);
+		setUpNumOfRecordsSummary(KEY_DIVINATIONS);
 	}
 	
+	@Override
+	protected void onPause()
+	{
+		super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+	
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
+		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+	}
+
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
 	{
@@ -43,12 +63,36 @@ public class Preferences extends PreferenceActivity implements OnSharedPreferenc
 		}
 		else if(key.equals(KEY_VIEW))
 		{
-			Toast.makeText(this, isMusicOn(this) + "", Toast.LENGTH_SHORT).show();
+			setUpViewSummary(key);
 		}
 		else if(key.equals(KEY_DIVINATIONS))
 		{
-			Toast.makeText(this, isMusicOn(this) + "", Toast.LENGTH_SHORT).show();
+			setUpNumOfRecordsSummary(key);
 		}
+	}
+
+	private void setUpNumOfRecordsSummary(String key)
+	{
+		String numOfRecordsSummary = getString(R.string.divination_summary);
+		String numOfRecordsValue = getStringValue(this, key, DEFAULT_VALUE_DIVINATIONS);
+		if(numOfRecordsValue.equals("-1"))
+		{
+			numOfRecordsValue = getString(R.string.infinite);
+		}
+		numOfRecordsSummary = numOfRecordsSummary + " " + getString(R.string.open_bracket) + numOfRecordsValue + getString(R.string.close_bracket);
+		numOfRecordsListPreference.setSummary(numOfRecordsSummary);
+	}
+
+	private void setUpViewSummary(String key)
+	{
+		String viewSummary = getString(R.string.hexagrams_view_preference_summary);
+		String viewValue = getString(R.string.current_value_view_on);
+		if(getStringValue(this, key, DEFAULT_VALUE_VIEW).equals("0"))
+		{
+			viewValue = getString(R.string.current_value_view_off);
+		}
+		viewSummary = viewSummary + " " + viewValue;
+		viewListPreference.setSummary(viewSummary);
 	}
 
 	public static boolean isMusicOn(Context context)
@@ -59,11 +103,6 @@ public class Preferences extends PreferenceActivity implements OnSharedPreferenc
 	public static String getStringValue(Context context, String key, String defaultValue)
 	{
 		return getSharedPreferences(context).getString(key, defaultValue);
-	}
-	
-	public static int getIntValue(Context context, String key, int defaultValue)
-	{
-		return getSharedPreferences(context).getInt(key, defaultValue);
 	}
 	
 	private static SharedPreferences getSharedPreferences(Context context)
