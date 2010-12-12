5edfcfe7127783e1162678d0a4637f318a79da53
hl <everbeyond@hotmail.com>
2010-12-11 22:08:51 -0500

    refactored code

--- src/iching/android/activities/Randomizer.java
+++ src/iching/android/activities/Randomizer.java
@@ -8,7 +8,6 @@ import org.anddev.andengine.engine.options.EngineOptions;
 import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.anddev.andengine.entity.scene.Scene;
-import org.anddev.andengine.entity.scene.background.ColorBackground;
 import org.anddev.andengine.entity.scene.transition.FadeTransitionScene;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.entity.util.FPSLogger;
